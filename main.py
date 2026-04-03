515088f91a8cbcd41891a77a01b04e312b891a83
themanprince <princeadigwe29@gmail.com>
2026-04-02 22:17:02 -0700

    added paystack js library for integration... did test running at templates/dashboard.html

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
 from routes.admin import AdminRouter
+from services.admin import AdminServices
+from db.db import get_db
+from sqlalchemy.orm import Session
 
 
 
@@ -22,8 +25,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def landing(request: Request):
-	return templates.TemplateResponse("index.html", {"request": request}) #type: ignore
+async def landing(request: Request, session:Session = Depends(get_db)):
+	room_types = await AdminServices.get_all_room_types(session=session)
+	return templates.TemplateResponse("index.html", {"request": request, "room_types": room_types}) #type: ignore
 
 
 app.include_router(AdminRouter)
